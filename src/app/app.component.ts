ac5282d885be989fc070a60e1c7ffb2d9c12985e
Rajesh Pandey <rajanme19@gmail.com>
2019-08-05 21:03:08 +0545

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ReturnStatement } from "@angular/compiler";
+import { variable } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'calculatormenu';
+  // regex: RegExp = new RegExp(/^\d*\.?\d{0,2}$/g);
+  title = "calculatormenu";
+  data: any[];
+  display = "0";
+  decimal = "0";
+  pValue = "0";
+  lastSign = "=";
+  lastNumber: number;
+  displaySign: string;
+  isDecimalCicked: boolean = true;
+  isSignClicked: boolean = false;
+  sign: string;
+
+  // isPercentClicked:boolean=false;
+  cancelClicked() {
+    console.log("cancel click");
+    this.display = "0";
+  }
+  cancelAll() {
+    console.log("cancel All click");
+    this.display = "0";
+    this.lastNumber = 0;
+    this.sign = "";
+  }
+
+  numberClicked(value: any) {
+    if (this.isSignClicked) {
+      this.display = "0";
+    }
+    if (this.display.toString() === "0.") {
+      this.display = this.display + value.toString();
+    } else if (+this.display === 0) {
+      this.display = value;
+    } else {
+      this.display = this.display.toString() + value.toString();
+    }
+    this.isSignClicked = false;
+  }
+
+  signClicked(sign: string) {
+    if (this.lastSign === "+") {
+      this.display = (this.lastNumber + +this.display).toString();
+    } else if (this.lastSign === "-") {
+      this.display = (this.lastNumber - +this.display).toString();
+    } else if (this.lastSign === "/") {
+      this.display = (this.lastNumber / +this.display).toString();
+    } else if (this.lastSign === "*") {
+      this.display = (this.lastNumber * +this.display).toString();
+    } else if (this.lastSign === "%") {
+      this.display = ((+this.lastNumber * +this.display) / 100).toString();
+    }
+    this.lastNumber = +this.display;
+    this.lastSign = sign;
+    this.isSignClicked = true;
+    this.displaySign = this.lastSign;
+  }
+  decimalClicked() {
+    if (this.display.toString().lastIndexOf(".") >= 0) {
+      return;
+    }
+    if (this.isSignClicked) {
+      this.display = "0.";
+    } else {
+      this.display = this.display.toString() + ".";
+    }
+    this.isSignClicked = false;
+  }
+
+  backClick() {
+    //  let a = [];
+    //     a = this.display.split("", this.display.length);
+
+    //     for (let i : number = 0; i < a.length; i++) {
+    //       console.log("push", this.data[i].push(a[i]));
+    //       // console.log('splice',this.data.splice(this.data[i],1,0));
+    //     }
+    //      console.log('dfghjk',this.data.splice(-1));
+    const len = this.display.toString().length;
+
+    this.display = this.display.toString().substring(0, len - 1);
+  }
+
+  plusminusClicked(){
+    
+    this.display = (+this.display * (-1)).toString();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule  } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
